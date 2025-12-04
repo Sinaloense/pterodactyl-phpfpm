7b39bfb5d08dd09f3c70008de6ec377a1dbd338c
Sinaloense <sinaloensemel@gmail.com>
2025-12-03 21:04:04 -0700

    Create html directory entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Output Current PHP Version
 php --version
 
+# Create html directory
+mkdir -p /home/container/html
+
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
