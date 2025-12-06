8623b3835c279d8aa24ba7c58cb39511dc100b58
Sinaloense <sinaloensemel@gmail.com>
2025-12-05 22:33:06 -0700

    $HOME used in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-cd /home/container
-
 # Output Current PHP Version
-php --version
+php -v
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ":$HOME$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
